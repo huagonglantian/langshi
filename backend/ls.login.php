f21752bcbc38df8c9015ca7c139dcbb794f84e2f
huagonglantian <huagonglantian@gmail.com>
2016-05-21 10:19:43 +0800

    0.01
    初始版本建立。
    160521

---
+++ backend/ls.login.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/3/31
+ * Time: 23:06
+ */
+
+//登录界面--尽量使用MVC结构
+//业务逻辑--
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/4/3
+ * Time: 13:13
+ */
+//加载登录HTML
+header("Location: ./frontend/index.html");
+?>
